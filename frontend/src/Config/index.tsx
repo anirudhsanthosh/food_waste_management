993ebcae3c295e58473379eb23eee9a3d497885a
Anirudh <anirudhsanthosh@gmail.com>
2023-12-03 21:12:29 +0530

    test

--- frontend/src/Config/index.tsx
+++ frontend/src/Config/index.tsx
@@ -1,6 +1,6 @@
-export class Configurations{
-    
-    public static baseUrl = "http://localhost:5000";
+export class Configurations {
+    // public static baseUrl = "http://localhost:5000";
+    public static baseUrl = "https://384c-2405-201-e017-d086-6a92-8560-5e93-602b.ngrok-free.app";
 
-    public static LOGIN_STATUS = 'svdasvavjhafjhvasjvjasd';
-} 
+    public static LOGIN_STATUS = "svdasvavjhafjhvasjvjasd";
+}
